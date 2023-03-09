4e39ae9e0c4e6b81c6b2854d6c30329e444656f8
EnGentech <engen.inyang@gmail.com>
2023-03-09 06:57:02 -0800

    argv program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 from sys import argv
+if __name__ == "__main__":
+    len_arg = len(argv)
 
-len_arg = len(argv)
-
-if len_arg == 1:
-    print("0 arguments.")
-elif len_arg > 0:
-    print("{:d} argument:".format(len(argv)))
-    for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+    if len_arg == 1:
+        print("0 arguments.")
+    elif len_arg > 0:
+        print("{:d} argument:".format(len(argv)))
+        for i in range(1, len(argv)):
+           print("{:d}: {}".format(i, argv[i]))
