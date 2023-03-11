48e51fc8b14c0def3d4519d82b98c956064c6303
EnGentech <engen.inyang@gmail.com>
2023-03-11 15:16:00 +0000

    matrix in python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for col in matrix:
         for row in col:
-            print("{:d}".format(row))
+            print("{:d}".format(row, end=" "))
         print()
