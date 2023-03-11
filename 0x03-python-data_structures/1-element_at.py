7c611c9b22dee8bfb944cd494fa61d5091b696ce
EnGentech <engen.inyang@gmail.com>
2023-03-11 13:04:45 +0000

    print list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for li in my_list:
-        if idx % 2 == 1:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
+        if idx % 2 == 0 and idx < len(my_list):
             return my_list[idx]
+        else:
+            return None
