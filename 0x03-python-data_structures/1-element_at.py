9280327a12974c1eacad1fcf1c66a4181695ed83
EnGentech <engen.inyang@gmail.com>
2023-03-11 12:50:17 +0000

    retrive elements

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+def element_at(my_list, idx):
+    for li in my_list:
+        if idx % 2 == 1:
+            return "None"
+        elif idx > len(my_list):
+            return "None"
+        else:
+            return my_list[idx]
