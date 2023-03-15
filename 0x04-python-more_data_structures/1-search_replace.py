e8222c38e05bdf5bcb5b1353ba1b62133f322c5d
EnGentech <engen.inyang@gmail.com>
2023-03-15 09:01:21 -0700

    replace list items

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
     new_list = my_list()
-    new_list = [replace if items == search for items in new_list]
+    new_list = [replace if items == search else items  for items in new_list]
     return new_list
