3a876b4e7e6f682df9d0d41ea3a9f2a4f02b8447
EnGentech <engen.inyang@gmail.com>
2023-03-28 06:58:43 -0700

    check class input

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class of square with raise value and type error"""
+
+
+class Square:
+    """defining conditions for size"""
+    __size = None
+    def __init__(self, size=0):
+        """initiallizing conditions for size"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
