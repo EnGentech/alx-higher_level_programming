a9c876474f3a7778ee1d5ceb139e552076e03a1d
EnGentech <engen.inyang@gmail.com>
2023-03-28 06:32:44 -0700

    class of square with init size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+defining a class of squares
+"""
+class Square:
+    """defining stage for class of square"""
+    __size = None
+
+    def __init__(self, size):
+        """initializing variable size"""
+        self.__size = size
