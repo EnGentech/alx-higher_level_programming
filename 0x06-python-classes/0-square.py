c2d0f640685c65eb86d28a59bd3f224f9cb88111
EnGentech <engen.inyang@gmail.com>
2023-03-28 05:58:20 -0700

    edited

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-defining empty class for squares
-"""
-class Square:
 """defining empty class for squares"""
+class Square:
+    """defining empty class for squares"""
     pass
