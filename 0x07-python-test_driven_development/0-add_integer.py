47dab69df3152896b153d05b5cf0a2e838d3c265
EnGentech <engen.inyang@gmail.com>
2023-04-09 05:30:05 -0700

    added documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""A program written to add
+the sum of two unknown variables"""
+
+
 def add_integer(a, b=98):
+    """defining a code to add two variables"""
     if type(a) is int or type(a) is float:
         if type(b) is int or type(b) is float:
             return int(a) + int(b)
