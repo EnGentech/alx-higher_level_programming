e36797b5083f2dee6ac44976c06bde52eedab470
EnGentech <engen.inyang@gmail.com>
2023-04-11 13:24:18 -0700

    check for object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+def is_same_class(obj, a_class):
+    """A program to return an instance of a
+    class base on obj and class provided"""
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
