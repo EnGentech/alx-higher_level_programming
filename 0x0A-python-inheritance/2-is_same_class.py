0585107da58298ff5ff6016ee5fbc97affcde2a2
EnGentech <engen.inyang@gmail.com>
2023-04-11 13:26:52 -0700

    check for object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+"""A program to return an instance of a
+class base on obj and class provided
+written by EnGentech"""
+
+
 def is_same_class(obj, a_class):
-    """A program to return an instance of a
-    class base on obj and class provided"""
+    """Function to define an obj type"""
 
     if type(obj) is a_class:
         return True
     else:
         return False
+# EnGentech
