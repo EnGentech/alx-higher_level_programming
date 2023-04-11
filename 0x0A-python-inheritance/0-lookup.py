011feec895518d6b1fd9b177acb38f1de7522993
EnGentech <engen.inyang@gmail.com>
2023-04-11 06:22:20 -0700

    loop up inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A program to return all attribut
+and methods in a function coded by EnGentech"""
+
+
+def lookup(obj):
+    """definting the function for the above"""
+    x = dir(obj)
+    return x
+# sign EnGentech
