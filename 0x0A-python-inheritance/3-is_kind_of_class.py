4867b15c4c309518916d1a97b277f3c659252ebd
EnGentech <engen.inyang@gmail.com>
2023-04-11 13:43:48 -0700

    isinstance program

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A program to check for an instance
+of a user input written by EnGentech"""
+
+
+def is_kind_of_class(obj, a_class):
+    """defining an instance of a class"""
+    return True if isinstance(obj, a_class) else False
+# EnGentech sign
