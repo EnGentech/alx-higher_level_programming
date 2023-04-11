7dea2ee2adc8ae35327cde365c8a75d789405e6a
EnGentech <engen.inyang@gmail.com>
2023-04-11 15:51:39 -0700

    edite

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,20 +5,16 @@ EnGentech"""
 
 
 class BaseGeometry:
-    """defining a class of BaseGeometry"""
-    
+    """BaseGeometry class defined"""
+
     def area(self):
-        """defining an area function"""
+        """this is a method for area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """funtion declaration"""
-        self.name = name
-        self.value = value
+        """Validating integer input"""
 
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
-# EnGentech sign
