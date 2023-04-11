f78a55213d3a1874dfc84c3f3855cbb6af923020
EnGentech <engen.inyang@gmail.com>
2023-04-11 14:19:16 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A program to raise an exception
+with no futher code written by
+EnGentech"""
+
+
+class BaseGeometry:
+    """defining a class for BaseGeometry"""
+    def area(self):
+        """definign a function for exception"""
+        raise Exception("area() is not implemented")
+# EnGentech sign
