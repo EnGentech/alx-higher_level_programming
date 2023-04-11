d4814d2a7d4824a86a89454359493dc3f571a4ee
EnGentech <engen.inyang@gmail.com>
2023-04-11 15:02:01 -0700

    edited

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,13 +6,14 @@ EnGentech"""
 
 class BaseGeometry:
     """defining a class of BaseGeometry"""
+
     def integer_validator(self, name, value):
         """funtion declaration"""
         self.name = name
         self.value = value
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError(name + " must be greater than 0")
 
     def area(self):
