9bafb3026274f7f448a68adc9e0426b769cc0b6b
EnGentech <engen.inyang@gmail.com>
2023-04-12 04:34:29 -0700

    json dumps

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A progam to dump an object in
+json written by EnGentech"""
+
+import json
+
+def to_json_string(my_obj):
+    """defining a json function"""
+
+    return json.dumps(my_obj)
+# EnGentech
