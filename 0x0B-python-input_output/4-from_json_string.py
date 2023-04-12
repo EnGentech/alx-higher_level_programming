fd4365fd8e4f6f0bfd128de2e4b5f135390a4812
EnGentech <engen.inyang@gmail.com>
2023-04-12 04:43:16 -0700

    load with json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+"""A program to load file content
+using json written by EnGentech"""
+
+
+def from_json_string(my_str):
+    """a function defining json load"""
+
+    rf_json = json.loads(my_str)
+    return rf_json
+# EnGentech sign
