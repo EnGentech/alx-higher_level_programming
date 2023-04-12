ef7c1aee8fc3d58fabbf398c17b05b4551e47012
EnGentech <engen.inyang@gmail.com>
2023-04-12 02:53:22 -0700

    read file content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A program to read the content of a txt file
+written by EnGentech"""
+
+
+def read_file(filename=""):
+    """function to read a file"""
+
+    with open(filename, encoding="utf-8") as ReadMe:
+        print(ReadMe.read())
+
+# EnGentech
