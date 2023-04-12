81b381f87f70e0f0d85f953ddd6e35431e5f7db1
EnGentech <engen.inyang@gmail.com>
2023-04-12 09:18:34 -0700

    append data by string search

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A program to append a string base
+on search value written by EnGentech"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function to append file base on string"""
+
+    with open(filename, mode="r", encoding="utf-8") as rd:
+        rd_line = rd.readline()
+
+    with open(filename, mode="w", encoding="utf-8") as rd:
+        for i in rd_line:
+            rd.write(i)
+            if search_string in i:
+                rd.write(new_string + "\n")
+# EnGentech sign
