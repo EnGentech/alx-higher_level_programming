5096acf9a1be48a1b5dda90ad246e94d5368ecb5
EnGentech <engen.inyang@gmail.com>
2023-04-17 03:09:44 -0700

    json dump code added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+import json
+
 """A program to create a class Base
 written by EnGentech"""
 
@@ -16,5 +19,12 @@ class Base:
     else:
         Base.__nb_objects += 1
         self.if = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """a funtion to dump into json"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
 # EnGentech sign
