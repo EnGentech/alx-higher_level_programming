1e56436b58dea3994132881041c5991e784bd309
EnGentech <engen.inyang@gmail.com>
2023-05-17 13:40:51 -0700

    modified task

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- selection from two tables with foreign key
 
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states)
 ORDER BY id ASC;
