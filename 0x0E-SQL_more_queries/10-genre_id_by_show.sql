540d95c06cd13943fe611c17f6086c2a610b8b21
EnGentech <engen.inyang@gmail.com>
2023-05-17 14:07:01 -0700

    modified tv.show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- list shows base on at least 1 genre link
 
-FROM tv_show.title, tv_show_genres.genre_id FROM tv_shows
+SELECT tv_show.title, tv_show_genres.genre_id FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
