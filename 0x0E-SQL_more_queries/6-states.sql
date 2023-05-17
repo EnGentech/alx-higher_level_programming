4fbd98c5d48f506e6d45989379a6dd947e2ea220
EnGentech <engen.inyang@gmail.com>
2023-05-17 12:29:31 -0700

    database with table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create a database with states as a table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT NOT NULL AUTO_INCREMENT DEFAULT 1 PRIMARY KEY,
+name VARCHAR(256)
+);
