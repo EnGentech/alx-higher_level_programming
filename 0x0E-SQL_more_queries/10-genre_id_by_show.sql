9297b09d179b5cfbde202f38cf53bdf0234b3a5a
EnGentech <engen.inyang@gmail.com>
2023-05-17 14:18:51 -0700

    hmmm

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- list shows base on at least 1 genre link
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres WHERE
-tv_shows.id = tv_show_genres.show_id
+FROM tv_shows, tv_show_genres 
+WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY by_shows.title, tv_show_genres.genre_id
 ASC;
