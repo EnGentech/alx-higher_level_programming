1cb73e6354a47c47e6aa7d0a4ce5fff795e0ada0
EnGentech <engen.inyang@gmail.com>
2023-06-17 08:50:50 -0700

    joining tables by rational database using foregin and primary key

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     mycursor = mydb.cursor()
 
     command = "SELECT cities.id, cities.name, states.name\
-    FROM cities INNER JOIN states WHERE ON cities.state_id = states.id\
+    FROM cities INNER JOIN states ON cities.state_id = states.id\
     ORDER BY cities.id ASC;"
     mycursor.execute(command)
 
