922164cdeb3dba32bf852e765011581f61c93729
EnGentech <engen.inyang@gmail.com>
2023-06-18 01:58:30 -0700

    query two tables with join function in sqlalchemy

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,10 +6,11 @@ on the database newly created"""
 from sqlalchemy import Column, String, Integer, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """Class definition of City"""
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True,\
+    id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
