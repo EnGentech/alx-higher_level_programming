8ac6262da6dd39bac05292ff17d976db32147fe4
EnGentech <engen.inyang@gmail.com>
2023-06-18 00:59:20 -0700

    created a class of city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is the beginning part of create
+a different class that will populate cities
+on the database newly created"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class City(Base):
+    """Class definition of City"""
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, unique=True,\
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey='states.id')
+
+# Coded by EnGentech
