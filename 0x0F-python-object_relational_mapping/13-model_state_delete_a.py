12e31a689b2166be466d1cfddbc70a218a311a79
EnGentech <engen.inyang@gmail.com>
2023-06-18 00:22:26 -0700

    delete all state with a lettter within itsself

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This code deals with sqlalchemy
+basically written to connect with our mysqldb
+get all the list of states present in there
+and list them in ascending order to out python snippet"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pas = sys.argv[2]
+    db = sys.argv[3]
+
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(uname, pas, db)
+    engine = create_engine(url)
+    Base.metadata.create_all(engine)
+    engine.connect()
+
+    session = sessionmaker()
+    local_session = session(bind=engine)
+
+    del_all_a_states = local_session.query(State)\
+        .filter(State.name.like("%a%")).all()
+
+    for i in del_all_a_states:
+        local_session.delete(i)
+        local_session.commit()
+
+# Coded by EnGentech
