1f892e6aaee66a3719f4a8990c33f79292edf043
EnGentech <engen.inyang@gmail.com>
2023-06-18 01:39:50 -0700

    modified model_city for redundant

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 class City(Base):
     """Class definition of City"""
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True,\
+    id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
