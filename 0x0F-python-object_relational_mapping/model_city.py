1d43ce94925d7adf4acab9053b9b7a07cc08d591
EnGentech <engen.inyang@gmail.com>
2023-06-18 01:43:54 -0700

    modified model_city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,18 +3,15 @@
 a different class that will populate cities
 on the database newly created"""
 
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
-
+from sqlalchemy import Column, String, Integer, ForeignKey
+from model_state import Base
 
 class City(Base):
     """Class definition of City"""
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True,
+    id = Column(Integer, autoincrement=True, unique=True,\
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
 
 # Coded by EnGentech
