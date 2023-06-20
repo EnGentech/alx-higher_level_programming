32e40618ab8dc9496538fbf1b761b46f46f566cb
Engr. Gentle Inyang <engen.inyang@gmail.com>
2023-06-20 02:23:25 +0100

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,14 +2,14 @@
 """In this advanced tasks, we deal more with relationship
 on how a table should relate with each other
 using the available resources on sqlalchemy"""
-
-import sys
-from relationship_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from relationship_city import City
-
 if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from relationship_city import City
+    from sqlalchemy.schema import Table
+
     uname = sys.argv[1]
     pas = sys.argv[2]
     db = sys.argv[3]
@@ -19,15 +19,12 @@ if __name__ == "__main__":
 
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    newState = State(name='California')
-    newCity = City(name='San Francisco')
-    newState.cities.append(newCity)
-
-    session.add(newState)
-    session.add(newCity)
+    session = Session(engine)
+    new_city = City(name='San Francisco')
+    new = State(name='California')
+    new.cities.append(new_city)
+    session.add_all([new, new_city])
     session.commit()
+    session.close()
 
 # Coded be EnGentech
