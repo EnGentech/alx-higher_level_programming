9fb9480353d0833f1b1aa7247f6c01056873084c
EnGentech <engen.inyang@gmail.com>
2023-06-29 11:44:54 -0700

    redo

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,16 +7,15 @@ from urllib import request, parse
 import sys
 
 if __name__ == "__main__":
-    get_val = 'X-Request-Id'
     url = sys.argv[1]
     email_address = sys.argv[2]
-    data = {'email': email_address}
-    data = parse.urlencode(data).encode('utf-8')
+    dat = {'email': email_address}
+    data = parse.urlencode(dat).encode('utf-8')
     reqst = request.Request(url, data=data)
 
     with request.urlopen(reqst) as file:
-        mail = file.read()
+        mail = file.read().decode('utf-8')
 
-    print("Your email is: {}".format(mail))
+        print(mail)
 
 # Coded by EnGentech
