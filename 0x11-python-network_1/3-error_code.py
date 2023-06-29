61bb1022e685c9a5ea0733741a8ce42c9f0524de
EnGentech <engen.inyang@gmail.com>
2023-06-29 12:09:27 -0700

    handle error if value is not meet

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This code uses a try except error catch
+to avoid a program crash when an error arises
+from HTTP.
+"""
+from urllib import request, error
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        url = argv[1]
+        with request.urlopen(url) as response:
+            content = response.read().decode("utf-8")
+            print(content)
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.status))
+
+# Coded by EnGentech
