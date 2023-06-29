46bbe27905deb119e01d7acd3b84dc2d4863007e
EnGentech <engen.inyang@gmail.com>
2023-06-28 17:25:09 -0700

    lets check this editing

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display the content of the web
-curl -sX POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
+curl "$1" -sX POST -d "email:test@gmail.com&subject:I will always be here for PLD"
