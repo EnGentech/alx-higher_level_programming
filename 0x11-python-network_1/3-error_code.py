b25c562f6db180303fe206aa13a940c283198b06
EnGentech <engen.inyang@gmail.com>
2023-06-29 12:27:15 -0700

    remodified

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-This code uses a try except .error catch
-to avoid a program crash when an .error arises
+This code uses a try except error catch
+to avoid a program crash when an error arises
 from HTTP.
 """
-from urllib import request
-import urllib.error as er
-from sys import argv
+import urllib.request
+import urllib.error
+import sys
 
 if __name__ == "__main__":
     try:
-        url = argv[1]
-        with request.urlopen(url) as response:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as response:
             content = response.read().decode("utf-8")
             print(content)
-    except er.HTTPError as err:
+    except urllib.error.HTTPError as err:
         print("Error code: {}".format(err.code))
 
 # Coded by EnGentech
