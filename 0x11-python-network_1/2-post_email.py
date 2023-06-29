d8d061a903e94a5a0a7c32935f7d167ff4540812
EnGentech <engen.inyang@gmail.com>
2023-06-29 09:22:27 -0700

    try this

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Get a response message from the server
+with a query string passed to the server
+basically an email
+"""
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    get_val = 'X-Request-Id'
+    url = sys.argv[1]
+    email_address = sys.argv[2]
+    data = {'email': email_address}
+    data = parse.urlencode(data).encode('utf-8')
+    reqst = request.Request(url, data)
+
+    with request.urlopen(reqst) as file:
+        mail = file.read()
+
+    print("Your email is: {}".format(mail))
+
+# Coded by EnGentech
