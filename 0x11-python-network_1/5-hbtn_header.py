3d0e347a91a3b6220f90ccf36c0b3ef5bacaf470
EnGentech <engen.inyang@gmail.com>
2023-06-30 11:16:49 -0700

    get header content

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Header request
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    content = requests.get(url)
+
+    print(content.headers['X-Request-Id'])
+
+# Coded by EnGentech
