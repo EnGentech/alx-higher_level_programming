ae5f7b77ad5fe25a5d848fd87953c9a1926de8cf
EnGentech <engen.inyang@gmail.com>
2023-06-30 03:56:20 -0700

    modifired

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,9 +7,9 @@ import requests
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    content = requests.get(url)
-    clas = content.text.__class__
-    txt = content.text
+    cont = requests.get(url)
+    clas = cont.text.__class__
+    txt = cont.text
 
     print("Body response:\n\t- type: {}\n\t- content: {}".format(clas, txt))
 
