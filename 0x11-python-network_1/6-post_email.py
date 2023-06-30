98188d425a5b41c0008197dc5049413060346ff4
EnGentech <engen.inyang@gmail.com>
2023-06-30 12:00:00 -0700

    post will mail

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,13 +2,14 @@
 """
 Request with post method
 """
+
 import requests
 from sys import argv
 
 if __name__ == '__main__':
     url = argv[1]
     mail = argv[2]
-    content = requests.post(url, data={'email':mail})
-    print(content)
+    content = requests.post(url, data={'email': mail})
+    print(content.text)
 
 # Coded by EnGentech
