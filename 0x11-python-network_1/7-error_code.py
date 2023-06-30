e8cfc1f2bf56c078e5f9906baf85dece78b79972
EnGentech <engen.inyang@gmail.com>
2023-06-30 14:57:47 -0700

    debugged

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,9 +4,11 @@ print conditionally on http status code
 """
 
 import requests
+from sys import argv
 
 if __name__ == "__main__":
-    reply = requests.get('http://engentech.tech/com')
+    url = argv[1]
+    reply = requests.get(url)
     code = reply.status_code
 
     if code >= 400:
