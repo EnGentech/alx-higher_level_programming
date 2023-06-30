4956f09605a1be9127aba4232fe2b2b18624af42
EnGentech <engen.inyang@gmail.com>
2023-06-30 03:27:18 -0700

    modified content

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-The use of requests.get to obtain content
+The use of requests.get to obtain info
 from the internet
 """
-
 import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
-content = requests.get(url)
-clas = content.text.__class__
-txt = content.text
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    content = requests.get(url)
+    clas = content.text.__class__
+    txt = content.text
 
-print("Body response:\n\t- type: {}\n\t- content: {}".format(clas, txt))
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(clas, txt))
+    print(content.status_code)
 
 # Coded by EnGentech
