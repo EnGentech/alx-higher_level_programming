23fe56b623db81703cdcc2dc6a86e2d339883140
EnGentech <engen.inyang@gmail.com>
2023-07-01 13:52:04 -0700

    recreated[6~

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Find the peak of the list provided"""
 
+
 def find_peak(list_of_integers):
     """A defined function to sort and list"""
     if list_of_integers:
         new_num = sorted(list_of_integers)
-        peak = new_num[len(num) - 1]
-        print(peak)
+        peak = new_num[len(list_of_integers) - 1]
+        return (peak)
     else:
-        print(None)
+        return (None)
 
 # Coded by EnGentech
