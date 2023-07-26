c8efdba33f0e682d7425bcdac03d98c439631705
EnGentech <engen.inyang@gmail.com>
2023-07-26 10:17:18 -0700

    repushed 

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,20 +6,20 @@ request(process.argv[2], function (err, _res, body) {
   if (err) {
     console.log(err);
   } else {
-    const my_dict = {};
+    const dict = {};
     body = JSON.parse(body);
 
     for (let i = 0; i < body.length; ++i) {
       const userId = body[i].userId;
       const completed = body[i].completed;
 
-      if (completed && !my_dict[userId]) {
-        my_dict[userId] = 0;
+      if (completed && !dict[userId]) {
+        dict[userId] = 0;
       }
 
-      if (completed) ++my_dict[userId];
+      if (completed) ++dict[userId];
     }
 
-    console.log(my_dict);
+    console.log(dict);
   }
 });
