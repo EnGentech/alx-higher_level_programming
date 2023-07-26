672b3f793d0bbf9b30cf757f744b8d167d66fa76
EnGentech <engen.inyang@gmail.com>
2023-07-26 15:24:47 -0700

    redo 

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const file = require('fs');
 
-let content = process.argv[3];
+const content = process.argv[3];
 
 file.writeFile(process.argv[2], content, err => {
   if (err) console.error(err);
